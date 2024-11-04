160fd489967731e48b410f5b3765fbbba3518dd0
dbdbddib <qudgns4475@gmail.com>
2024-11-04 20:45:38 +0900

    stomp study start
    stomp study start

---
+++ stomp/src/main/java/com/ybh/stomp/StompRoomDto.java
@@ -0,0 +1,25 @@
+package com.ybh.stomp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StompRoomDto {
+    private String roomName;
+    private String roomId;
+    private final List<StompWebSocketSession> stompWebSocketSessions = new ArrayList<>();
+
+    public Integer getCount() {
+        return stompWebSocketSessions.size();
+    }
+}
