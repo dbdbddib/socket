ce6d5f91da6fb92c5363dd524ae41f3f4d5e8810
dbdbddib <qudgns4475@gmail.com>
2024-11-05 22:16:53 +0900

    stomp 메세지 전송, 퇴장 버튼 적용, 참여자 수 적용. 브라우저 닫을때는 퇴장 안됨, 브라우저 닫기 시 퇴장 처리
    stomp 메세지 전송, 퇴장 버튼 적용, 참여자 수 적용. 브라우저 닫을때는 퇴장 안됨, 브라우저 닫기 시 퇴장 처리

---
+++ stomp/src/main/java/com/ybh/stomp/StompChatController.java
@@ -0,0 +1,42 @@
+package com.ybh.stomp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+
+@Slf4j
+@Controller
+public class StompChatController {
+    @Autowired
+    private SimpMessageSendingOperations msgTempate;
+
+    @Autowired
+    private StompRoomService stompRoomService;
+
+    @MessageMapping("/stomp/message")
+    public void message(StompMessageDto stompMessageDto) {
+        log.info("/stomp/message => roomId:{}, msgType:{}, writer:{}, message:{}"
+                , stompMessageDto.getRoomId()
+                , stompMessageDto.getMsgType()
+                , stompMessageDto.getWriter()
+                , stompMessageDto.getMessage()
+        );
+        StompRoomDto stompRoom = stompRoomService.findByRoomId(stompMessageDto.getRoomId());
+        if (stompRoom == null) {
+            return;
+        }
+        if ( StompMessageDto.StompMessageType.ENTER == stompMessageDto.getMsgType() ) {
+            stompRoom.setCount(stompRoom.getCount() + 1);
+        } else if ( StompMessageDto.StompMessageType.OUT == stompMessageDto.getMsgType() ) {
+            stompRoom.setCount(stompRoom.getCount() - 1);
+        }
+        if( stompRoom.getCount() < 1 ) {
+            stompRoomService.deleteByRoomId(stompMessageDto.getRoomId());
+        } else {
+            msgTempate.convertAndSend("/sub/stomp/room/" + stompMessageDto.getRoomId()
+                    , stompMessageDto);
+        }
+    }
+}
